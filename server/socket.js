94d5cff2894dc5a28063278050f2d585c969df09
Chase Sillevis <chase@sillevis.net>
2014-06-13 17:29:11 +0200

    Clarity

--- server/socket.js
+++ server/socket.js
@@ -1,31 +1,40 @@
+// Initiate
 var loopback = require('loopback');
 var app = module.exports = loopback();
+app.boot(__dirname);
 
+// Socket stuff
 var Primus = require('primus');
 var Emitter = require('primus-emitter');
 var Res = require('primus-resource');
 var http = require('http');
 
+// Resources
 var Location = loopback.getModel('Location');
 
+// Start up
 var server = http.createServer().listen(app.get('socket.port')),
     primus = new Primus(server, { transformer: 'websockets', parser: 'JSON' });
 
+// Set up primus defaults
 primus.use('emitter', Emitter).use('multiplex', 'primus-multiplex').use('resource', Res);
 
+// Listen for connections
 console.log('Socket listening on %s', app.get('socket.port'));
 primus.on('connection', function (spark) {
   console.log('Detected connection');
 
-  // receive incoming sport messages
+  // receive incoming location updates
   spark.on('location', function (data) {
     console.log('location', data); // => ping-pong
     console.log('Creating location');
 
+    // Create the entries
     Location.create(data, function (result) {
       console.log('Location updated: ', result);
     });
 
+    // Send the update to interested parties (auto-updating their map)
     spark.send('map', 'Data received');
   });
 });
